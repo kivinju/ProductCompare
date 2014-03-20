00289bbd918f5567e8e129fdad9234bcba9fa629
Apple <Apple@AppletekiMacBook-Air-3.local>
2014-03-20 12:50:55 +0800

    ss  added service

---
+++ src/cn/edu/nju/apoc/service/ManageBidding.java
@@ -0,0 +1,28 @@
+package cn.edu.nju.apoc.service;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import cn.edu.nju.apoc.entity.Bidding;
+
+@Service
+public class ManageBidding {
+	
+	public void addBidding(Bidding bidding){
+		
+	}
+	
+	public void deleteBidding(Bidding bidding){
+		
+	}
+	
+	public ArrayList<Bidding> getAllBidding(Bidding bidding){
+		return null;
+	}
+	
+	public void updateBidding(Bidding bidding){
+		
+	}
+	
+}
